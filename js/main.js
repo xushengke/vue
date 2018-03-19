841c34d1765b3c34e6a847912b64f3a644135cea
SHENGKE XU <xushengke94@gmail.com>
2018-03-19 13:43:10 -0400

    video stuff

--- js/main.js
+++ js/main.js
@@ -12,16 +12,30 @@ const vm = new Vue({
 
     anchorOff : true,
 
-    deliciousFruit : [
-      {name : "apples", flavor : "tangy!"},
-      {name : "peaches", flavor : "sweet"},
-      {name : "grapes", flavor : "squishy"}
-    ]
+    moviedata : [
+      {name : "star wars the force", thumb : "forceawakens.jpg", vidsource : "forceawakens.mp4", description : "dedeaedad"},
+      {name : "stranger things", thumb : "strangerthings.jpg", vidsource : "stranerthings.mp4", description : "sssss"},
+      {name : "avenger", thumb : "avengers.jpg", vidsource : "avengers.mp4", description : "dedeaedad"},
+    ],
+
+    moviename : "movie name goes here",
+    moviedescription : "movie description goes here",
+    moviesource : "null"
   },
 
   methods : {
-    logFruit(e) {
-      console.log(e.currentTarget);
+    logvideo(e) {
+      e.preventDefault();
+      //console.log(e.currentTarget);
+
+      //find the fref name of the video and use it to filter the collection
+      datakey = e.currentTarget.href.substring(e.currentTarget.href.lastIndexOf('/')
+    + 1);
+
+      currentData = this.moviedata.filter(video => video.vidsource === datakey);
+      this.moviename = currentData[0].name;
+      this.moviedescription = currentData[0].description;
+      this.moviesource = datakey;
     }
   }
 });
